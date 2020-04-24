f6fed4856517d09b25b7342e749bf8463ec46ace
user_roman <user_roman@localhost.localdomain>
2020-04-24 06:39:24 -0400

    Updates

---
+++ precache-manifest.cfd62eb3387c54855f57c68a736fd7d5.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "5dd3976cb5d61e2e561f2a46b916f377",
+    "url": "/front-dubbing-project/static/media/fa-regular-400.5dd3976c.woff"
+  },
+  {
+    "revision": "efbaa518e29cb4eb980f",
+    "url": "/front-dubbing-project/static/css/main.bb5ccaa1.chunk.css"
+  },
+  {
+    "revision": "61e72a8790bfaf0820eb",
+    "url": "/front-dubbing-project/static/js/runtime~main.61e72a87.js"
+  },
+  {
+    "revision": "d24b72205d57854d1bbae8103779d71d",
+    "url": "/front-dubbing-project/static/js/2.aed96571.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aed9657141eaff4d5e6f",
+    "url": "/front-dubbing-project/static/js/2.aed96571.chunk.js"
+  },
+  {
+    "revision": "8b68e78f68893a490ff9cd25487b0687",
+    "url": "/front-dubbing-project/static/media/theater-logo.8b68e78f.png"
+  },
+  {
+    "revision": "d9d17590c975aad1be0ddab673f9c769",
+    "url": "/front-dubbing-project/static/media/fa-brands-400.d9d17590.eot"
+  },
+  {
+    "revision": "4b115e1153a9ea339d6a0bb284cc8ed3",
+    "url": "/front-dubbing-project/static/media/fa-brands-400.4b115e11.woff2"
+  },
+  {
+    "revision": "b90365bccdabd68c6c03902b4b141f09",
+    "url": "/front-dubbing-project/static/media/fa-brands-400.b90365bc.woff"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "/front-dubbing-project/static/media/fa-brands-400.c39278f7.ttf"
+  },
+  {
+    "revision": "80533988ff5fecd5be26557d08ce8237",
+    "url": "/front-dubbing-project/static/media/fa-brands-400.80533988.svg"
+  },
+  {
+    "revision": "414ff5daad323a1c47c5177d4bd29674",
+    "url": "/front-dubbing-project/static/media/fa-regular-400.414ff5da.eot"
+  },
+  {
+    "revision": "65779ebcc35604a25c2ba77309c5b8af",
+    "url": "/front-dubbing-project/static/media/fa-regular-400.65779ebc.woff2"
+  },
+  {
+    "revision": "efbaa518e29cb4eb980f",
+    "url": "/front-dubbing-project/static/js/main.efbaa518.chunk.js"
+  },
+  {
+    "revision": "f6c6f6c8cb7784254ad00056f6fbd74e",
+    "url": "/front-dubbing-project/static/media/fa-regular-400.f6c6f6c8.ttf"
+  },
+  {
+    "revision": "e7e957c87c454bccaa3bf9fdaa6874f8",
+    "url": "/front-dubbing-project/static/media/fa-regular-400.e7e957c8.svg"
+  },
+  {
+    "revision": "b5596f4d339f99e3d69bc41be78db962",
+    "url": "/front-dubbing-project/static/media/fa-solid-900.b5596f4d.eot"
+  },
+  {
+    "revision": "462806316fea535a6a57651bc2b000b0",
+    "url": "/front-dubbing-project/static/media/fa-solid-900.46280631.woff2"
+  },
+  {
+    "revision": "61969d433bf265b9717a6c357a1e04e4",
+    "url": "/front-dubbing-project/static/media/fa-solid-900.61969d43.woff"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "/front-dubbing-project/static/media/fa-solid-900.b70cea03.ttf"
+  },
+  {
+    "revision": "82905d8d1c06969df11c8c378e9bdd4c",
+    "url": "/front-dubbing-project/static/media/fa-solid-900.82905d8d.svg"
+  },
+  {
+    "revision": "100b38fa184634fc89bd07a84453992c",
+    "url": "/front-dubbing-project/static/media/Montserrat-Light.100b38fa.ttf"
+  },
+  {
+    "revision": "9c46095118380d38f12e67c916b427f9",
+    "url": "/front-dubbing-project/static/media/Montserrat-Regular.9c460951.ttf"
+  },
+  {
+    "revision": "a98626e1aef6ceba5dfc1ee7112e235a",
+    "url": "/front-dubbing-project/static/media/Montserrat-Medium.a98626e1.ttf"
+  },
+  {
+    "revision": "aed9657141eaff4d5e6f",
+    "url": "/front-dubbing-project/static/css/2.ed2e1188.chunk.css"
+  },
+  {
+    "revision": "6337a6703a497dfc07b359ece498bac6",
+    "url": "/front-dubbing-project/index.html"
+  }
+];
